9bb2c93f3afda4ef04d413c14017a6b06abfd59b
Mohammed Aljanahi <mohammed.aljanaahi@hungerstation.com>
2026-02-16 11:05:15 +0400

    chore : implementing grpc

--- serviceb/grpc/server.go
+++ serviceb/grpc/server.go
@@ -1,1 +1,19 @@
 package grpc
+
+import (
+	"context"
+
+	pb "github.com/maljanahi99/weather_tracker/protos"
+)
+
+type Server struct {
+	pb.UnimplementedWeatherServiceServer
+}
+
+func (s *Server) GetWeather(ctx context.Context, req *pb.GetWeatherRequest) (*pb.WeatherResponse, error) {
+	return &pb.WeatherResponse{
+		Current: &pb.Current{
+			Temperature: req.Latitude + req.Longitude,
+		},
+	}, nil
+}
