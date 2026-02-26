5e6be85a67f58fb252b62eab2a5e8ff82cb19b43
Mohammed Aljanahi <mohammed.aljanaahi@hungerstation.com>
2026-02-26 10:23:42 +0400

    chore : implement simple http server in servicea

--- go.mod
+++ go.mod
@@ -3,12 +3,13 @@ module github.com/maljanahi99/weather_tracker
 go 1.25.6
 
 require (
+	github.com/gorilla/mux v1.8.1
 	google.golang.org/grpc v1.78.0
 	google.golang.org/protobuf v1.36.10
 )
 
 require (
-	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/lib/pq v1.11.2 // indirect
 	golang.org/x/net v0.47.0 // indirect
 	golang.org/x/sys v0.38.0 // indirect
 	golang.org/x/text v0.31.0 // indirect
---
+++ servicea/http.go
@@ -0,0 +1,49 @@
+package servicea
+
+import (
+	"context"
+	"encoding/json"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/mux"
+	pb "github.com/maljanahi99/weather_tracker/protos"
+)
+
+type HTTPServer struct {
+	ServiceB pb.WeatherServiceClient
+}
+
+func (s *HTTPServer) Weather(router *mux.Router) {
+	router.
+		HandleFunc("/weather", s.weatherHandler).
+		Methods("GET").
+		Name("weather")
+}
+
+func (s *HTTPServer) weatherHandler(w http.ResponseWriter, r *http.Request) {
+	ctx, cancel := context.WithTimeout(r.Context(), 3*time.Second)
+	defer cancel()
+
+	resp, err := s.ServiceB.GetWeather(ctx, &pb.GetWeatherRequest{
+		Latitude:  52.52,
+		Longitude: 13.41,
+	})
+
+	if err != nil {
+		http.Error(w, "grpc call failed: "+err.Error(), http.StatusBadGateway)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	_ = json.NewEncoder(w).Encode(resp)
+}
+
+func StartServer(serviceB pb.WeatherServiceClient) error {
+	router := mux.NewRouter()
+
+	s := &HTTPServer{ServiceB: serviceB}
+	s.Weather(router)
+
+	return http.ListenAndServe(":8080", router)
+}
